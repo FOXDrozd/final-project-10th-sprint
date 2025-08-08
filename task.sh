78d92d4512c9fb9f483a8491c39ba42aefca6eed
Vladlena Drozdova <drozdova.vl03@gmail.com>
2025-08-08 11:47:02 +0400

    Fixed bash script

--- task.sh
+++ task.sh
@@ -11,7 +11,7 @@ mkdir -p task/dir1 task/dir2 task/dir3/dir4
 
 
 # изменяем текущую директорию на task
-# cd task
+cd task
 
 # создаём пустой файл task/dir2/empty
 
@@ -72,7 +72,7 @@ grep  "dir" ./"Практическое задание" | sort
 
 # меняем текущую директорию на родительскую для task
 
-cd /home/$USER
+cd ~/task
 
 # удаляем директорию task со всем содержимым
 
