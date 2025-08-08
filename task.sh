6f752d4ca73c6eeea73e0227b51f6324a5607027
Vladlena Drozdova <drozdova.vl03@gmail.com>
2025-08-08 11:41:03 +0400

    Fixed bash script

--- task.sh
+++ task.sh
@@ -7,12 +7,8 @@
 #   dir3
 #       dir4
 
-mkdir task
-cd /task
-mkdir dir1 dir2 dir3
-cd dir3
-mkdir dir4
-cd ..
+mkdir -p task/dir1 task/dir2 task/dir3/dir4 
+cd task
 
 # изменяем текущую директорию на task
 # cd task
@@ -25,21 +21,20 @@ touch ./dir2/empty
 # #!/bin/bash
 # echo "$1, привет!"
 
-echo "$1, привет" > ./dir2/hello.sh
+echo '#!/bin/bash
+echo "$1, привет!"' > ./dir2/hello.sh
 
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 
 chmod 764 ./dir2/hello.sh
 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
-cd ./dir2
-ls > list.txt
+ls ./dir2 > list.txt
 cat list.txt
-cd ..
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
 
-cp -r -T ./dir2 ./dir3/dir4
+cp -r ./dir2/* ./dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
@@ -57,21 +52,20 @@ export NAME="Всем студентам"
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
 
-echo "$NAME, привет" > ./dir2/hello.sh
-cat ./dir2/hello.sh  >> ./dir1/summary.txt
+./dir2/hello.sh "Всем студентам" >> ./dir1/summary.txt
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
 
-mv ./dir1/summary.txt ./"практическое задание"
+mv ./dir1/summary.txt ./"Практическое задание"
 
 # выводим на консоль содержимое файла task/Практическое задание
 
-cat ./"практическое задание"
+cat ./"Практическое задание"
 
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
 
-grep  "dir" ./"практическое задание" | sort
+grep  "dir" ./"Практическое задание" | sort
 
 # меняем текущую директорию на родительскую для task
 
